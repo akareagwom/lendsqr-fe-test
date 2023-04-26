582ce018fe34a87fcc510295b3e3c6e9a460dad5
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-04-26 18:37:03 +0100

    added navbar content

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,13 +1,29 @@
 import logo from '../../assets/Union.svg'
 import './navbar.scss'
-
+import {
+    IoMdNotificationsOutline,} from 'react-icons/io'
+import {AiOutlineCaretDown} from 'react-icons/ai'    
+    //  IoSearchOutline,
+    
 const Navbar = () => {
     return (  
         <>
-        <div>
+        <div className='navbar'>
             <div className='nav-head'>
                 <img src={logo} alt=""  />
                 <h1>lendsqr</h1>
+            </div>
+            <div>
+                <input type="search" name="" placeholder='search' id="" />
+            </div>
+            <div className='persona'>
+                <h5>Docs</h5>
+                <IoMdNotificationsOutline/>
+                <div className='profile'>
+                    <img src="profile.png" alt=""/>
+                    <h6>Adedeji</h6>
+                    <AiOutlineCaretDown/>
+                </div>
             </div>
         </div>
         </>
