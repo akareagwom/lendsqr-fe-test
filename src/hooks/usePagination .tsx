f2c36309b64b2f4e9d64385fd6679c3fb0914345
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-07-30 19:59:58 +0100

    set siblingCount default value

--- src/hooks/usePagination .tsx
+++ src/hooks/usePagination .tsx
@@ -1,12 +1,19 @@
+import { useMemo } from "react";
+
+interface Props {
+    totalCount: number;
+    currentPage: number;
+    pageSize: number;
+    siblingCount: number;
+}
+
 export const usePagination = ({
     //passing the props
     totalCount,
     currentPage,
     pageSize,
-    OnPageChange,
-    siblingCount
+    siblingCount=1}:Props) =>{
 
-}) =>{
-    
+    const paginationRange = useMemo(()=>{},[totalCount,currentPage,pageSize,siblingCount])
 
 }
