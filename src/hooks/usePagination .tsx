069d10b6de88380d33b1d5924c750775c9629beb
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-07-31 15:49:53 +0100

    specified the arguments

--- src/hooks/usePagination .tsx
+++ src/hooks/usePagination .tsx
@@ -5,8 +5,9 @@ interface Props {
     currentPage: number;
     pageSize: number;
     siblingCount: number;
-    start:number;
-    end:number;
+    // start:number;
+    // end:number;
+    num:number;
 }
 
 export const usePagination = ({
@@ -20,13 +21,17 @@ export const usePagination = ({
         //totalPageCount
         const totalPageCount = Math.ceil(totalCount/pageSize);
         //Custom range
-        const range = ({start,end}:Props)=>{
+        const range = (start:number,end:number)=>{
             let length= end-start+1;
             return Array.from({length},(_,idx)=>idx+start);
     
         }
         const totalPageNumbers= siblingCount +5;
-        
+
+        if (totalPageNumbers >= totalPageCount){
+            return range(1,totalPageCount);
+
+        }
 
 
 }
